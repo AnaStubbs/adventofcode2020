1fefa6adc56d0bdbf7812687250d12626dad7004
Anne Campbell <CampbellAH@cardiff.ac.uk>
2020-12-14 14:47:19 +0000

    formatting

--- 01/01.py
+++ 01/01.py
@@ -1,9 +1,9 @@
 expenses = {}
 
-with open('01.txt' )as input_file:
+with open('01.txt')as input_file:
     for line in input_file:
         num = int(line)
-        if num in expenses:
-            print(f"Part 1: {num*expenses[num]}")
+        if num in expenses:  # matching number already seen - do calc
+            print(f"Part 1: {num * expenses[num]}")
         else:
             expenses[2020 - num] = num
