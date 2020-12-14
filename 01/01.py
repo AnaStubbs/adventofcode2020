c804306cea6a91efb127fdab79c2142473526d32
Anne Campbell <CampbellAH@cardiff.ac.uk>
2020-12-14 14:44:19 +0000

    star 1

---
+++ 01/01.py
@@ -0,0 +1,9 @@
+expenses = {}
+
+with open('01.txt' )as input_file:
+    for line in input_file:
+        num = int(line)
+        if num in expenses:
+            print(f"Part 1: {num*expenses[num]}")
+        else:
+            expenses[2020 - num] = num
